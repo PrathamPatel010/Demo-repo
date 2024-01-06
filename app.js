4f933300cd769f69e328b56beb78d7d34c1ba067
Pratham <ppratham812@gmail.com>
2024-01-06 18:04:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.listen('3000',()=>{
+    console.log(`Server is listening on port 3000`);
+})
+
+app.get('/',(req,res)=>{
+    res.send(`<h1>Mock API Up & Running</h1>`);
+})
